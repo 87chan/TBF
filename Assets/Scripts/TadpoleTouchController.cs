d38ee28758a30bd6f58223ae2dcd00a54d4785c4
Hikeshi <test@gmai.com>
2018-09-13 01:27:42 +0900

    とりあえずバカ実装でマルチタップ対応したつもり

--- Assets/Scripts/TadpoleTouchController.cs
+++ Assets/Scripts/TadpoleTouchController.cs
@@ -22,10 +22,12 @@ public class TadpoleTouchController : MonoBehaviour
         Debug.Assert(this.Tadpole,"おたまじゃくしが見つかりません");
         this.boxCollider = GetComponent<Collider2D>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
+        //#todo 正気を疑うレベルで汚いが緊急時なので、無理やりマルチタップとエディタ上での処理をifdefで分けている
+#if UNITY_EDITOR
         TouchInfo info = AppUtil.GetTouch();
 
         if (info == TouchInfo.Began)
@@ -34,7 +36,7 @@ public class TadpoleTouchController : MonoBehaviour
 
             // 自身が持つ矩形内がタップされていたときのみタップ開始とする.
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(StartPos);
-            if(this.boxCollider.OverlapPoint(worldPos))
+            if (this.boxCollider.OverlapPoint(worldPos))
             {
                 bTapTrigger = true;
             }
@@ -53,6 +55,42 @@ public class TadpoleTouchController : MonoBehaviour
                 this.Tadpole.MoveDirection(direction);
             }
         }
+#else
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, 0.0f);
+        
+            if ((int)touch.phase == (int)TouchInfo.Began)
+            {
+                // 自身が持つ矩形内がタップされていたときのみタップ開始とする.
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
+                if (this.boxCollider.OverlapPoint(worldPos))
+                {
+                    bTapTrigger   = true;           //タップトリガ.
+                    this.fingerId = touch.fingerId; //指IDを記録.
+                    StartPos      = touchPos;       //タップスタート位置を記憶.
+                }
+            }
+            else if ((int)touch.phase == (int)TouchInfo.Ended && bTapTrigger)
+            {
+                //指IDが一致した場合のみ移動処理.
+                if(this.fingerId == touch.fingerId)
+                {
+                    bTapTrigger = false;
+                    // 指を動かした場合にのみ、オタマジャクシを移動させる。
+                    if (StartPos != touchPos)
+                    {
+                        Vector3 direction = (StartPos - touchPos).normalized;
+                        // 移動方向の確定.おたまじゃくしに伝える.
+                        this.Tadpole.MoveDirection(direction);
+                    }
+                }
+            }
+        }
+
+#endif
     }
 
     Tadpole Tadpole; //操作対象のオタマジャクシ.
@@ -60,5 +98,7 @@ public class TadpoleTouchController : MonoBehaviour
     Vector3 StartPos;       //タップ開始位置.
     bool bTapTrigger;       //タップされている
     Collider2D boxCollider; //自分が持っている当たり矩形    
+
+    int fingerId;   // タップした指ID.
 
 }
